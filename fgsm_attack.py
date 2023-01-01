26504098f8978be4b36b1e8d92802b7d78fc73bc
mhu3 <mhu3@wpi.edu>
2023-01-01 02:10:39 +0000

    add l2 fgsm attack

--- fgsm_attack.py
+++ fgsm_attack.py
@@ -12,13 +12,15 @@ class FGSM():
         self.model = model
 
     def linf_attack(self, samples, labels, eps=0.001, iteration=10):
+        # normalization factor  49, 92
         # Binary cross entropy (0/1)
         loss_object = tf.keras.losses.BinaryCrossentropy()
 
         # Change label size to [batch_size, 1]
         labels = labels[:, np.newaxis]
 
-        pair_input = tf.Variable(samples, dtype=tf.float32)
+        pairs_ori = tf.Variable(samples, dtype=tf.float32)
+        pairs_adv = tf.Variable(samples, dtype=tf.float32)
 
         # Iterative FGSM
         for i in tqdm(range(iteration)):
@@ -26,14 +28,14 @@ class FGSM():
             with tf.GradientTape() as tape:
                 
                 # Watch the input
-                tape.watch(pair_input)
+                tape.watch(pairs_adv)
                 
                 # Compute loss
-                prediction = self.model(pair_input)
+                prediction = self.model(pairs_adv)
                 loss = loss_object(labels, prediction)
 
             # Get the gradients of the loss w.r.t to the input
-            gradients = tape.gradient(loss, pair_input)
+            gradients = tape.gradient(loss, pairs_adv)
             # Get the sign of the gradients to create the perturbation
             signed_grads = tf.sign(gradients)
 
@@ -50,8 +52,81 @@ class FGSM():
 
             # Perturb the input
             signed_grads = tf.convert_to_tensor(np_grads)
-            pair_input = pair_input + eps * signed_grads
+            pairs_adv = pairs_adv + eps * signed_grads
+            
+            # compute the difference between the original and the perturbed trajectory
+            diff = pairs_adv.numpy() - pairs_ori.numpy()
 
-        return pair_input.numpy()
+            # Clip the diff to be in the range of [-1/49, 1/49], [-1/92, 1/92] on x, y 
+            diff[:, :, :, 0] = np.clip(diff[:, :, :, 0], -1/49, 1/49)
+            diff[:, :, :, 1] = np.clip(diff[:, :, :, 1], -1/92, 1/92)
+
+            diff = tf.convert_to_tensor(diff)
+            
+            # Add the difference to the original input
+            pairs_adv = pairs_ori + diff
+
+        return pairs_adv.numpy()
+
+    def l2_attack(model, pairs, labels, eps=0.1, iteration=10, p_norm = 2):
+        # normalization factor  49, 92
+        # Binary cross entropy (0/1)
+        loss_object = tf.keras.losses.BinaryCrossentropy()
+        
+        # Change label size to [batch_size, 1]
+        labels = labels[:, np.newaxis]
+
+        pairs_ori = tf.Variable(pairs, dtype=tf.float32)
+        pairs_adv = tf.Variable(pairs, dtype=tf.float32)
+        
+        # Iterative FGSM
+        for i in tqdm(range(iteration)):
+            # Compute current loss
+            with tf.GradientTape() as tape:
+
+                # Watch the input
+                tape.watch(pairs_adv)
+
+                # Compute loss
+                prediction = model(pairs_adv)
+                loss = loss_object(labels, prediction)
+
+            # Get the gradients of the loss w.r.t to the input
+            gradients = tape.gradient(loss, pairs_adv)
+
+            # get the l2-norm of the gradients
+            norm = tf.norm(tf.reshape(gradients, [gradients.shape[0], -1]), ord=p_norm, axis=1)
+            norm = tf.reshape(norm, [gradients.shape[0], 1, 1, 1])
+
+            # l2 norm of the gradients
+            l2_grads = gradients / norm
+
+            # Constraint on sequential data
+            # we should not change the padding
+            l2_grads = l2_grads.numpy()
+            for i in range(l2_grads.shape[0]):
+                # find rows with all 0s
+                trajs, rows = np.where( np.sum(l2_grads[i], axis=2) == 0 )
+                l2_grads[i, trajs, rows, :] = 0
+
+                # avoid changing time
+                l2_grads[i, :, :, 2] = 0
+
+            # Perturb the input
+            l2_grads = tf.convert_to_tensor(l2_grads)
+            pairs_adv = pairs_adv + eps * l2_grads
+            
+            # compute the difference between the original and the perturbed trajectory
+            diff = pairs_adv.numpy() - pairs_ori.numpy()
+
+            # Clip the diff to be in the range of [-1/49, 1/49], [-1/92, 1/92] on x, y 
+            diff[:, :, :, 0] = np.clip(diff[:, :, :, 0], -1/49, 1/49)
+            diff[:, :, :, 1] = np.clip(diff[:, :, :, 1], -1/92, 1/92)
+
+            diff = tf.convert_to_tensor(diff)
+            
+            # Add the difference to the original input
+            pairs_adv = pairs_ori + diff
+
+        return pairs_adv.numpy()
     
-    # def l2_attack(model, pairs, labels, eps=0.1, iteration=10, p_norm = 2):
